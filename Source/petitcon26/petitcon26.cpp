c789cc27f023b3065ff2351cfe1217bbd7e221cd
Daisuke Suzuki <bboydaisuke@gmail.com>
2026-07-18 22:58:51 +0900

    Initial commit

---
+++ Source/petitcon26/petitcon26.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "petitcon26.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, petitcon26, "petitcon26" );
